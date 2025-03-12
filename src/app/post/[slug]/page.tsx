ec47e37fe3f3ae33e6542459b5e72da93d76c986
Antonin SIMON <antonin.simon@epsi.fr>
2025-03-12 12:19:31 +0100

    ajustement du Breadcrumb

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -72,7 +72,7 @@ export default async function PostPage({
           </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbPage>{post.name}</BreadcrumbPage>
+            <BreadcrumbPage>{post.name.length > 42 ? `${post.name.substring(0, 42)}...` : post.name}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
